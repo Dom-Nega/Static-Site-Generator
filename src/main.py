c9c5d8f869696201652d88cf70cead9d483bc5ac
Dom-Nega <domnega92@gmail.com>
2025-02-17 07:57:17 -0800

    updates to Static-Site-Generator

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from textnode import TextNode, TextType
+from inlinenodes import text_to_textnodes
 
 def main():
     node = TextNode("this is a test", TextType.BOLD, "www.hello.com")
     print(node)
-
 
 if __name__ == "__main__":
     main()
